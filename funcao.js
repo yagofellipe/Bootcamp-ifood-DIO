57f238c6c89ff78e9b4c84bc6057711aa0d9e89e
yagofellipe <yagofellipe09@gmail.com>
2023-10-12 13:04:04 -0300

    1

--- funcao.js
+++ funcao.js
@@ -3,4 +3,13 @@ function torrar()
     console.log("torrando pão");
 }
 
-torrar()
+function enviarDados()
+{
+    let nomeDoBanco = "banco-de-dados";
+    console.log("salvando dados em: " + nomeDoBanco);
+
+    if (3 === 3)
+    {
+        console.log("senha validada");
+    }
+}
